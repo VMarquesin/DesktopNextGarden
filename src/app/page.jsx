7e72f8a27a46d18064d4686cc0dfaacb5ad4e134
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-08-19 20:57:56 -0300

    atualização

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,27 +4,22 @@ import styles from "./page.module.css";
 export default function Home() {
    return (
       <div className="containerGlobal">
-         <h1 className={styles.tituloHomePage}>Aula Next JS</h1>
-         <h2>Exemplo h2</h2>
-         <h3>Exemplo h3</h3>
-         <p>Texto parágrafo</p>
-
          <div className={styles.containerImagem}>
             <Image
-               src="/download.jfif"
+               src="/background-wood-home.jpg"
                width={500}
                height={500}
                alt="Imagem"
                className={styles.imagemHomePage}
             />
 
-            <Image
+            {/* <Image
                src="/download.jfif"
                width={50}
                height={50}
                alt="Imagem"
                className={styles.imagemHomePage}
-            />
+            /> */}
          </div>
       </div>
    );
--- src/app/sobre/page.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./page.module.css";
-
-export default function Sobre() {
-   return (
-      <div className="containerGlobal">
-         <h1 className={styles.tituloSobre}>Sobre</h1>
-      </div>
-   );
-}
