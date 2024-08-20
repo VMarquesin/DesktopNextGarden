2a7837dc8c046fcff4eea17250eb741d668ef4cc
MARCOS FELIPE GAVA DA CRUZ <161661597+FelpaGava@users.noreply.github.com>
2024-08-19 21:30:39 -0300

    Tela Login
    Inicio da criação da tela de Login

--- src/app/usuarios/login/page.jsx
+++ src/app/usuarios/login/page.jsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
 export default function Login() {
    return (
-      <div className="containerGlobal">
-         <h1>Login</h1>
+      <div className="LoginContainer">
+         <div className="BackgroundLogin">
+            <h1>Garden</h1>
+            <p>Acesse sua conta:</p>
+         </div>
+         <div className={styles.BackgroundImageLogin}>
+            <Image
+               src="/ImageLogin.png"
+               width={500}
+               height={500}
+               alt="Imagem"
+               className={styles.imagemHomePage}
+            />
+         </div>
       </div>
    );
 }
