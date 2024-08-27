a21d294e10aef88c4bacb8cbc28ee25feda48e5b
MARCOS FELIPE GAVA DA CRUZ <161661597+FelpaGava@users.noreply.github.com>
2024-08-26 22:26:39 -0300

    Atualização

---
+++ src/app/usuarios/login/page.jsx
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Login() {
+   return (
+      <div className={styles.LoginContainer}>
+         {/* <div className={styles.LoginContent}> */}
+
+         <div className={styles.LoginAcess}>
+            <div className={styles.LoginForm}>
+               <label htmlFor="nomeProjeto">GARDEN</label>
+               <label htmlFor="Acesse" className={styles.AcesseSuaConta}>
+                  Acesse sua conta:
+               </label>
+
+               <form>
+                  <div className={styles.FormGroup}>
+                     <label htmlFor="username">Nome de usuário:</label>
+                     <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Digite seu nome de usuário"
+                        className={styles.InputField}
+                     />
+                  </div>
+
+                  <div className={styles.FormGroup}>
+                     <label htmlFor="password">Senha:</label>
+                     <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Digite sua senha"
+                        className={styles.InputField}
+                     />
+                  </div>
+
+                  <button type="submit" className={styles.SubmitButton}>
+                     Entrar
+                  </button>
+               </form>
+            </div>
+         </div>
+
+         <div className={styles.containerButton}>
+            <button type="submit" className={styles.ButtonCadastro}>
+               Cadastrar-se
+            </button>
+         </div>
+
+         <div>
+            <Image
+               src="/ImageLogin.png"
+               width={500}
+               height={500}
+               alt="Imagem"
+               className={styles.BackgroundImageLogin}
+            />
+         </div>
+         {/* </div> */}
+      </div>
+   );
+}
