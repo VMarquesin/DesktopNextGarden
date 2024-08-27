529937f11614f9d357a11d1004b7324eb0aae2cf
MARCOS FELIPE GAVA DA CRUZ <161661597+FelpaGava@users.noreply.github.com>
2024-08-26 22:00:34 -0300

    Att

--- src/app/usuarios/login/page.jsx
+++ src/app/usuarios/login/page.jsx
@@ -3,44 +3,53 @@ import styles from "./page.module.css";
 
 export default function Login() {
    return (
-     <div className={styles.LoginContainer}>
-         <div className={styles.LoginContent}>
+      <div className={styles.LoginContainer}>
+         {/* <div className={styles.LoginContent}> */}
+
+         <div className={styles.LoginAcess}>
             <div className={styles.LoginForm}>
-              <div className={styles.LoginAcess}> 
                <label htmlFor="nomeProjeto">GARDEN</label>
-               <label htmlFor="Acesse" className={styles.AcesseSuaConta}>Acesse sua conta:</label>
-              </div>        
-               
+               <label htmlFor="Acesse" className={styles.AcesseSuaConta}>
+                  Acesse sua conta:
+               </label>
+
                <form>
                   <div className={styles.FormGroup}>
-                     <label htmlFor="username">Nome de usuário</label>
-                     <input 
-                        type="text" 
-                        id="username" 
-                        name="username" 
-                        placeholder="Digite seu nome de usuário" 
+                     <label htmlFor="username">Nome de usuário:</label>
+                     <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        placeholder="Digite seu nome de usuário"
                         className={styles.InputField}
                      />
                   </div>
-                  
+
                   <div className={styles.FormGroup}>
-                     <label htmlFor="password">Senha</label>
-                     <input 
-                        type="password" 
-                        id="password" 
-                        name="password" 
-                        placeholder="Digite sua senha" 
+                     <label htmlFor="password">Senha:</label>
+                     <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        placeholder="Digite sua senha"
                         className={styles.InputField}
                      />
                   </div>
-                  
+
                   <button type="submit" className={styles.SubmitButton}>
                      Entrar
                   </button>
                </form>
             </div>
-            
-            <div className={styles.BackgroundImageLogin}>
+         </div>
+
+         <div className={styles.containerButton}>
+            <button type="submit" className={styles.ButtonCadastro}>
+               Cadastrar-se
+            </button>
+         </div>
+
+         <div>
             <Image
                src="/ImageLogin.png"
                width={500}
@@ -48,8 +57,8 @@ export default function Login() {
                alt="Imagem"
                className={styles.BackgroundImageLogin}
             />
-            </div>
          </div>
+         {/* </div> */}
       </div>
    );
 }
