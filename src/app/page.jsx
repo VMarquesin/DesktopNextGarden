713464d3eb0a6d0f9d938efc1f5cddfd824827b9
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-09-01 18:07:34 -0300

    Commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -75,9 +75,10 @@ export default function Home() {
                      <button
                         data-target="#anotacoes"
                         onClick={() => setTela(1)}
+                        className={Tela === 1 ? styles.activeButton : " "}
                      >
                         <Image
-                           src="/icones/Note.png"
+                           src="/icones/Note.svg"
                            alt="Note"
                            width={20}
                            height={20}
@@ -87,9 +88,13 @@ export default function Home() {
                      </button>
                   </li>
                   <li>
-                     <button data-target="#diario" onClick={() => setTela(2)}>
+                     <button
+                        data-target="#diario"
+                        onClick={() => setTela(2)}
+                        className={Tela === 2 ? styles.activeButton : " "}
+                     >
                         <Image
-                           src="/icones/diario.png"
+                           src="/icones/diario.svg"
                            alt="Diario"
                            width={20}
                            height={20}
@@ -102,9 +107,10 @@ export default function Home() {
                      <button
                         data-target="#Exercícios"
                         onClick={() => setTela(3)}
+                        className={Tela === 3 ? styles.activeButton : " "}
                      >
                         <Image
-                           src="/icones/exercicios.png"
+                           src="/icones/exercicios.svg"
                            alt="Exercícios"
                            width={20}
                            height={20}
@@ -135,9 +141,13 @@ export default function Home() {
                      )} */}
                   </li>
                   <li>
-                     <button data-target="#lembrete" onClick={() => setTela(4)}>
+                     <button
+                        data-target="#lembrete"
+                        onClick={() => setTela(4)}
+                        className={Tela === 4 ? styles.activeButton : " "}
+                     >
                         <Image
-                           src="/icones/lembrete.png"
+                           src="/icones/lembrete.svg"
                            alt="tiktok"
                            width={20}
                            height={20}
@@ -151,9 +161,10 @@ export default function Home() {
                      <button
                         data-target="#dashboard"
                         onClick={() => setTela(5)}
+                        className={Tela === 5 ? styles.activeButton : " "}
                      >
                         <Image
-                           src="/icones/dashboard.png"
+                           src="/icones/dashboard.svg"
                            alt="Dashboard"
                            width={20}
                            height={20}
