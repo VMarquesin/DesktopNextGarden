180a014cc57ab30da83d10a66040c3d2649ff49f
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-09-01 19:43:30 -0300

    Commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -37,7 +37,7 @@ export default function Home() {
                         <li>
                            <div className={styles.notifications}>
                               <Image
-                                 src="/icones/Notificacao.png"
+                                 src="/icones/Notification.svg"
                                  alt="sino de notificação"
                                  width={20}
                                  height={20}
