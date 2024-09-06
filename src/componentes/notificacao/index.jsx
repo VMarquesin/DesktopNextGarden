c632d1ebbf2c62d3085014656177e1e4a4945deb
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-09-05 21:39:20 -0300

    commit

--- src/componentes/notificacao/index.jsx
+++ src/componentes/notificacao/index.jsx
@@ -1,3 +1,68 @@
+import { useState, useEffect } from "react";
+
+import styles from "./index.module.css"
+import Image from "next/image";
+
+export default function Notificacoes() {
+  const [mostrarNotificacoes, setMostrarNotificacoes] = useState(false);
+
+  // Exemplo de lista de notificações
+  const notificacoes = [
+    { id: 1, mensagem: "Nova mensagem de paciente X" },
+    { id: 2, mensagem: "Sessão agendada com paciente Y" },
+    { id: 3, mensagem: "Atividade concluída por paciente Z" },
+  ];
+
+  // Alterna entre mostrar ou esconder a lista de notificações
+  const toggleNotificacoes = () => {
+    setMostrarNotificacoes(!mostrarNotificacoes);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+       if (notificationRef.current && !notificationRef.current.contains(event.target)) {
+          setShowNotifications(false);
+       }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+       document.removeEventListener('mousedown', handleClickOutside);
+    };
+ }, []);
+  return (
+    <div>
+      {/* Ícone de notificações */}
+      <button  className={styles.icon} onClick={toggleNotificacoes}>
+        <Image
+          src="/icones/Notification.svg"
+          alt="Notificações"
+          width={20}
+          height={20}
+         
+        />
+      </button>
+
+      {/* Lista de notificações que aparece quando o estado mostrarNotificacoes é true */}
+      {mostrarNotificacoes && (
+        <div className={styles.listaNotificacoes}>
+          {notificacoes.length > 0 ? (
+            notificacoes.map((notificacao) => (
+              <div key={notificacao.id} className={styles.notificacao}>
+                {notificacao.mensagem}
+              </div>
+            ))
+          ) : (
+            <div className={styles.notificacao}>Sem notificações</div>
+          )}
+        </div>
+      )}
+    </div>
+  )
+};
+
+
+
 // import { useState, useEffect, useRef } from "react";
 
 // export default function NotificacaoButton() {
