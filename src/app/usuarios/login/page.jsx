90c0398b38a0c25903aa0e7dcbf2918992332a9b
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-09-14 14:30:39 -0300

    Commit

--- src/app/usuarios/login/page.jsx
+++ src/app/usuarios/login/page.jsx
@@ -1,69 +1,131 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Login() {
+import React, { useState } from "react";
+import Image from "next/image";
+
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+import styles from "./page.module.css";
+import api from "../../../services/api";
+
+function Login() {
+   const router = useRouter();
+
+   const [login, setLogin] = useState("");
+   const [senha, setSenha] = useState("");
+
+   function handleSubmit(event) {
+      event.preventDefault();
+      logar();
+   }
+
+   async function logar(event) {
+      try {
+         const dados = {
+            usu_email: login,
+            usu_senha: senha,
+         };
+
+         const response = await api.post("/usuarios/login", dados);
+
+         if (response.data.sucesso == true) {
+            const usuario = response.data.dados;
+            const objLogado = {
+               id: usuario.usu_id,
+               nome: usuario.usu_nome,
+               acesso: usuario.usu_adm,
+            };
+            // signin(JSON.stringify(objLogado));
+            localStorage.clear();
+            localStorage.setItem("user", JSON.stringify(objLogado));
+            router.push("/"); // é possível direcionar de acordo com a situação
+         } else {
+            alert(
+               "Erro: " +
+                  error.response.data.mensagem +
+                  "\n" +
+                  error.response.data.dados
+            );
+         }
+      } catch (error) {
+         if (error.response) {
+            alert(
+               error.response.data.mensagem + "\n" + error.response.data.dados
+            );
+         } else {
+            alert(error);
+         }
+      }
+   }
    return (
       <div className={styles.LoginContainer}>
          {/* <div className={styles.LoginContent}> */}
 
-
          <div className={styles.LoginAcess}>
             <div className={styles.LoginForm}>
+               <button type="submit" className={styles.ButtonCadastro}>
+                  CRIAR CONTA
+               </button>
 
-            <button type="submit" className={styles.ButtonCadastro}>
-                      CRIAR CONTA
-                  </button>
+               <div className={styles.Titulo}>
+                  <label htmlFor="nomeProjeto">GARDEN</label>
+                  <label htmlFor="Acesse" className={styles.AcesseSuaConta}>
+                     Acesse sua conta:
+                  </label>
+               </div>
 
-              <div className={styles.Titulo}>
-                <label htmlFor="nomeProjeto">GARDEN</label>
-                 <label htmlFor="Acesse" className={styles.AcesseSuaConta}>
-                  Acesse sua conta:
-                </label>
-               </div>   
-
-
-               <form>
-                <div className={styles.FormGroup}>
+               <form id="form" className={styles.form} onSubmit={handleSubmit}>
+                  <div className={styles.FormGroup}>
                      <input
                         type="text"
-                        id="username"
-                        name="username"
-                        placeholder="Usuário"
+                        id="email"
+                        name="email"
+                        placeholder="E-mail"
+                        onChange={(v) => setLogin(v.target.value)}
+                        value={login}
                         className={styles.InputField}
                      />
                      <Image
-                         src="/Icones/Usuario.svg"
-                         width={25}
-                         height={25}
-                         alt="Icone Usuário"
-                         className={styles.Icons}
-                        />
+                        src="/Icones/Usuario.svg"
+                        width={25}
+                        height={25}
+                        alt="Icone Usuário"
+                        className={styles.Icons}
+                     />
                   </div>
-                  
+
                   <div className={styles.FormGroup}>
                      <input
                         type="password"
                         id="password"
                         name="password"
                         placeholder="Senha"
+                        onChange={(v) => setSenha(v.target.value)}
+                        value={senha}
                         className={styles.InputField}
                      />
-                       <Image
-                         src="/Icones/OcultaSenha.svg"
-                         width={25}
-                         height={25}
-                         alt="Icone Senha"
-                         className={styles.Icons}
-                        />
+                     <Image
+                        src="/Icones/OcultaSenha.svg"
+                        width={25}
+                        height={25}
+                        alt="Icone Senha"
+                        className={styles.Icons}
+                     />
                   </div>
 
-                  <label htmlFor="Esqueceu sua senha?" className={styles.EsqueceuSuaSenha}>Esqueceu sua senha?</label>
+                  <label
+                     htmlFor="Esqueceu sua senha?"
+                     className={styles.EsqueceuSuaSenha}
+                  >
+                     Esqueceu sua senha?
+                  </label>
 
                   <button type="submit" className={styles.SubmitButton}>
                      ENTRAR
                   </button>
-                  </form>
-             </div>
+               </form>
+            </div>
          </div>
 
          <div className={styles.containerButton}></div>
@@ -80,3 +142,6 @@ export default function Login() {
       </div>
    );
 }
+export default Login;
+
+// trocar botão criar conta por um link
---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'http://10.67.22.146:3306' // ip e porta do servidor
+    // lab6 10.67.22.146
+    // lab4 10.67.22.144
+});
+
+export default api;
