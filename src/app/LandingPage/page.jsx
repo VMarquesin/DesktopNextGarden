09bab3aa2ab0ecba4e61e068aff23f19c646e5d6
VMarquesin <157024346+VMarquesin@users.noreply.github.com>
2024-09-17 22:49:32 -0300

    Commit

--- src/app/LandingPage/page.jsx
+++ src/app/LandingPage/page.jsx
@@ -5,94 +5,133 @@ export default function Page() {
    return (
       <div className={styles.container}>
          <div className={styles.BackgroundHeader}>
-         <div className={styles.header}>
-         <div className={styles.topBar}>
-            <Image
-               className={styles.logo}
-               src="/images/LogoWhite.png"
-               width={700}
-               height={300}
-               alt="Picture of the author"
-            />
+            <div className={styles.header}>
+               <div className={styles.topBar}>
+                  <Image
+                     className={styles.logo}
+                     src="/images/LogoWhite.png"
+                     width={700}
+                     height={300}
+                     alt="Picture of the author"
+                  />
                   <nav className={styles.nav}>
                      <ul>
-                     <li><a href="#paciente">PACIENTE</a></li>
-                     <li><a href="#seguranca">SEGURANÇA</a></li>
-                     <li><a href="#psicologo">PSICÓLOGO</a></li>
-                     <li><a href="#login">LOGIN</a></li>
-                     </ul>                   
+                        <li>
+                           <a href="#paciente">PACIENTE</a>
+                        </li>
+                        <li>
+                           <a href="#seguranca">SEGURANÇA</a>
+                        </li>
+                        <li>
+                           <a href="#psicologo">PSICÓLOGO</a>
+                        </li>
+                        <li>
+                           <a href="#login">LOGIN</a>
+                        </li>
+                     </ul>
                   </nav>
                </div>
             </div>
-        
 
-         <div className={styles.hero}>
-            <p> Seu bem estar está em suas mãos e o Garden está aqui para te ajudar</p>
-            <button className={styles.saibaMais}>Saiba Mais</button>
-         </div>
-         </div>
-
-
-{/* Seção paciente */}
-         <section id="paciente" className={styles.pacienteSection}>
-         <div className={styles.BackgroundPaciente}>
-                  <Image 
-                     src="/images/BackgroundPaciente.png"       //Imagem de fundo parte PACIENTE
-                     alt="Escada das emoções"
-                     width={743}
-                     height={2389}
-                  />
+            <div className={styles.hero}>
+               <p>
+                  {" "}
+                  Seu bem estar está em suas mãos e o Garden está aqui para te
+                  ajudar
+               </p>
+               <button className={styles.saibaMais}>Saiba Mais</button>
             </div>
+         </div>
 
-            <div className={styles.pacienteIntro}>
-             <p>No aplicativo Garden, você tem a oportunidade excepcional de elevar seu autoconhecimento a níveis inimagináveis, embarcando em uma jornada única de exploração dos seus pensamentos, emoções e convicções mais profundas. Através de uma colaboração verdadeira, 
-               ajudamos você a acessar suas crenças mais arraigadas e a trabalhar de forma consciente para impulsionar sua evolução pessoal, espiritual e profissional.
-             </p>
-           </div>
-           
-            <div className={styles.pacienteCards}>
-               <div className={styles.card}>
-                  {/* <Image
-                     src="/image1.png"
-                     alt="Imagem 1"
-                     width={150}
-                     height={150}
-                  /> */}
-                  <p>Descrição 1</p>
+         {/* Seção paciente */}
+         <section id="paciente" className={styles.pacienteSection}>
+            <div className={styles.BackgroundPaciente}>
+               <div className={styles.pacienteIntro}>
+                  <p>
+                     No aplicativo Garden, você tem a oportunidade excepcional
+                     de elevar seu autoconhecimento a níveis inimagináveis,
+                     embarcando em uma jornada única de exploração dos seus
+                     pensamentos, emoções e convicções mais profundas. Através
+                     de uma colaboração verdadeira, ajudamos você a acessar suas
+                     crenças mais arraigadas e a trabalhar de forma consciente
+                     para impulsionar sua evolução pessoal, espiritual e
+                     profissional.
+                  </p>
                </div>
-               <div className={styles.card}>
-                  {/* <Image
-                     src="/image2.png"
-                     alt="Imagem 2"
-                     width={150}
-                     height={150}
-                  /> */}
-                  <p>Descrição 2</p>
-               </div>
-               <div className={styles.card}>
-                  {/* <Image
-                     src="/image3.png"
-                     alt="Imagem 3"
-                     width={150}
-                     height={150}
-                  /> */}
-                  <p>Descrição 3</p>
+
+               {/* <Image
+                  src="/images/BackgroundPaciente.png" //Imagem de fundo parte PACIENTE
+                  alt="Escada das emoções"
+                  width={743}
+                  height={2389}
+               /> */}
+
+               <div className={styles.pacienteCards}>
+                  <div className={styles.card}>
+                     <Image
+                        src="/images/diario.svg"
+                        alt="Imagem 1"
+                        width={450}
+                        height={450}
+                     />
+                  </div>
+                  <div className={styles.card}>
+                     <Image
+                        src="/images/emocao.svg"
+                        alt="Imagem 2"
+                        width={450}
+                        height={450}
+                     />
+                  </div>
+                  <div className={styles.card}>
+                     <Image
+                        src="/images/conhecimento.svg"
+                        alt="Imagem 3"
+                        width={450}
+                        height={450}
+                     />
+                  </div>
                </div>
             </div>
          </section>
 
-         <section id="seguranca" className={styles.segurancaSection}>
+         <section id="seguranca">
             <div className={styles.segurancaContent}>
-               <div className={styles.segurancaImage}></div>
-               <div className={styles.textContent}>
-                  <h2>Expresse sem medo</h2>
-                  <p>Texto explicativo sobre segurança.</p>
+               <div className={styles.segurancaImage}>
+                  <div className={styles.telemovel}>
+                     <Image
+                        src="/images/telemovel.svg"
+                        alt="Imagem 3"
+                        width={750}
+                        height={750}
+                     />
+                  </div>
+                  <div className={styles.textContent}>
+                     <h2 className={styles.primeiro}>Expresse</h2>
+                     <h2>sem medo</h2>
+                     <p>
+                        Com criptografia de ponta a ponta todas mensagens e
+                        aúdios estão protegidos. Utilizamos tecnologias
+                        avançadas de segurança para proteger seus dados
+                        pessoais. Sua privacidade é nossa prioridade máxima, e
+                        nunca compartilharemos suas informações sem sua
+                        permissão. Fique tranquilo enquanto você se concentra em
+                        sua jornada de bem-estar e autoconhecimento com nosso
+                        aplicativo.
+                     </p>
+                  </div>
                </div>
             </div>
          </section>
 
          <section className={styles.divWithGradient}>
-            <p>Texto dentro da div com gradiente.</p>
+            <p>
+               Integrando o Garden em sua prática, você estará oferecendo uma
+               ferramenta adicional de apoio aos seus pacientes, complementando
+               seu trabalho terapêutico. Com recursos práticos e conteúdos
+               complementares, o Garden é um recurso valioso para ajudar seus
+               pacientes a alcançarem seus objetivos de saúde mental.
+            </p>
          </section>
 
          <section id="psicologo" className={styles.psicologoSection}>
@@ -118,7 +157,10 @@ export default function Page() {
          </section>
 
          <section className={styles.divWithGradient}>
-            <p>Texto dentro da div com gradiente.</p>
+            <p>
+               Estamos aqui para ser o solo fértil onde suas aspirações podem
+               florescer. Venha mergulhar nas maravilhas do seu jardim interior.
+            </p>
          </section>
 
          <footer className={styles.footer}>
