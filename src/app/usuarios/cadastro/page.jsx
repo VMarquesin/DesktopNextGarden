6804bf662e8cc05accadca3308bc370cf37fbc7d
MARCOS FELIPE GAVA DA CRUZ <161661597+FelpaGava@users.noreply.github.com>
2024-09-23 19:20:22 -0300

    Atualizações

--- src/app/usuarios/cadastro/page.jsx
+++ src/app/usuarios/cadastro/page.jsx
@@ -20,16 +20,14 @@ export default function Cadastro() {
       usu_nick: "",
       usu_email: "",
       usu_senha: "",
-      usu_dt_nasc: "",
+      confSenha: "",
       psi_cnpj: "",
+      end_cep: "",
       end_logradouro: "",
       end_num: "",
       end_bairro: "",
-      end_complemento: "",
       cid_id: "0",
-      cli_cel: "",
       uf: "0",
-      confSenha: "",
    });
 
    const valDefault = styles.formControl;
@@ -88,7 +86,7 @@ export default function Cadastro() {
          validado: valDefault,
          mensagem: [],
       },
-      Apelido: {
+      apelido: {
          validado: valDefault,
          mensagem: [],
       },
@@ -100,11 +98,15 @@ export default function Cadastro() {
          validado: valDefault,
          mensagem: [],
       },
-      nascimento: {
+      confSenha: {
          validado: valDefault,
          mensagem: [],
       },
       cnpj: {
+         validado: valDefault,
+         mensagem: [],
+      },
+      cep: {
          validado: valDefault,
          mensagem: [],
       },
@@ -120,19 +122,11 @@ export default function Cadastro() {
          validado: valDefault,
          mensagem: [],
       },
-      complemento: {
+      cidade: {
          validado: valDefault,
          mensagem: [],
       },
-      celular: {
-         validado: valDefault,
-         mensagem: [],
-      },
-      senha: {
-         validado: valDefault,
-         mensagem: [],
-      },
-      confSenha: {
+      estado: {
          validado: valDefault,
          mensagem: [],
       },
@@ -165,47 +159,47 @@ export default function Cadastro() {
       return testeResult;
    }
 
-   function validaNascimento() {
-      // Obtém a data atual
-      const hoje = new Date();
-      const anoAtual = hoje.getFullYear();
+   // function validaNascimento() {  //CONFIRMAR SE IRA USAR!!!!!!!!!!!!!!!
+   //    // Obtém a data atual
+   //    const hoje = new Date();
+   //    const anoAtual = hoje.getFullYear();
 
-      // Converte a data de nascimento para um objeto Date
-      const dataNascimentoObj = new Date(usuario.usu_dt_nasc);
+   //    // Converte a data de nascimento para um objeto Date
+   //    const dataNascimentoObj = new Date(usuario.usu_dt_nasc);
 
-      let objTemp = {
-         validado: valSucesso, // css referente ao estado de validação
-         mensagem: [], // array de mensagens de validação
-      };
+   //    let objTemp = {
+   //       validado: valSucesso, // css referente ao estado de validação
+   //       mensagem: [], // array de mensagens de validação
+   //    };
 
-      if (usuario.usu_dt_nasc === "") {
-         objTemp.validado = valErro;
-         objTemp.mensagem.push("O preenchimento da data é obrigatório");
-      } else if (dataNascimentoObj > hoje) {
-         objTemp.validado = valErro;
-         objTemp.mensagem.push("A data de nascimento não pode ser futura");
-      } else {
-         // Calcula a idade
-         const anoNascimento = dataNascimentoObj.getFullYear();
-         const idade = anoAtual - anoNascimento;
-         if (idade > 100) {
-            objTemp.validado = valErro;
-            objTemp.mensagem.push("A idade não pode ser superior a 100 anos");
-         }
-         if (idade < 14) {
-            objTemp.validado = valErro;
-            objTemp.mensagem.push("A idade não pode ser inferior a 14 anos");
-         }
-      }
+   //    if (usuario.usu_dt_nasc === "") {
+   //       objTemp.validado = valErro;
+   //       objTemp.mensagem.push("O preenchimento da data é obrigatório");
+   //    } else if (dataNascimentoObj > hoje) {
+   //       objTemp.validado = valErro;
+   //       objTemp.mensagem.push("A data de nascimento não pode ser futura");
+   //    } else {
+   //       // Calcula a idade
+   //       const anoNascimento = dataNascimentoObj.getFullYear();
+   //       const idade = anoAtual - anoNascimento;
+   //       if (idade > 100) {
+   //          objTemp.validado = valErro;
+   //          objTemp.mensagem.push("A idade não pode ser superior a 100 anos");
+   //       }
+   //       if (idade < 14) {
+   //          objTemp.validado = valErro;
+   //          objTemp.mensagem.push("A idade não pode ser inferior a 14 anos");
+   //       }
+   //    }
 
-      setValida((prevState) => ({
-         ...prevState, // mantém os valores anteriores
-         nascimento: objTemp, // atualiza apenas o campo 'nome'
-      }));
+   //    setValida((prevState) => ({
+   //       ...prevState, // mantém os valores anteriores
+   //       nascimento: objTemp, // atualiza apenas o campo 'nome'
+   //    }));
 
-      const testeResult = objTemp.mensagem.length === 0 ? 1 : 0;
-      return testeResult;
-   }
+   //    const testeResult = objTemp.mensagem.length === 0 ? 1 : 0;
+   //    return testeResult;
+   // }
 
    function checkEmail(email) {
       return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
@@ -236,66 +230,66 @@ export default function Cadastro() {
       return testeResult;
    }
 
-   function validaCpf() {
-      let objTemp = {
-         validado: valSucesso,
-         mensagem: [],
-      };
+   // function validaCpf() {
+   //    let objTemp = {
+   //       validado: valSucesso,
+   //       mensagem: [],
+   //    };
 
-      // Remove todos os caracteres não numéricos
-      const cpf = usuario.usu_cpf.replace(/\D/g, "");
+   //    // Remove todos os caracteres não numéricos
+   //    const cpf = usuario.usu_cpf.replace(/\D/g, "");
 
-      if (cpf.length < 11) {
-         objTemp.validado = valErro;
-         objTemp.mensagem.push("O CPF precisa ter 11 dígitos");
-      } else if (/^(\d)\1{10}$/.test(cpf)) {
-         objTemp.validado = valErro;
-         objTemp.mensagem.push("O CPF digitado é inválido");
-      } else if (validaDigitosCPF(cpf) === false) {
-         objTemp.validado = valErro;
-         objTemp.mensagem.push("O CPF digitado é inválido");
-      }
+   //    if (cpf.length < 11) {
+   //       objTemp.validado = valErro;
+   //       objTemp.mensagem.push("O CPF precisa ter 11 dígitos");
+   //    } else if (/^(\d)\1{10}$/.test(cpf)) {
+   //       objTemp.validado = valErro;
+   //       objTemp.mensagem.push("O CPF digitado é inválido");
+   //    } else if (validaDigitosCPF(cpf) === false) {
+   //       objTemp.validado = valErro;
+   //       objTemp.mensagem.push("O CPF digitado é inválido");
+   //    }
 
-      setValida((prevState) => ({
-         ...prevState, // mantém os valores anteriores
-         cpf: objTemp, // atualiza apenas o campo 'nome'
-      }));
+   //    setValida((prevState) => ({
+   //       ...prevState, // mantém os valores anteriores
+   //       cpf: objTemp, // atualiza apenas o campo 'nome'
+   //    }));
 
-      const testeResult = objTemp.mensagem.length === 0 ? 1 : 0;
-      return testeResult;
-   }
+   //    const testeResult = objTemp.mensagem.length === 0 ? 1 : 0;
+   //    return testeResult;
+   // }
 
-   function validaDigitosCPF(cpf) {
-      // Valida os dígitos verificadores
-      let soma = 0;
-      let resto;
-      for (let i = 1; i <= 9; i++) {
-         soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
-      }
-      resto = (soma * 10) % 11;
-      if (resto === 10 || resto === 11) {
-         resto = 0;
-      }
-      if (resto !== parseInt(cpf.substring(9, 10))) {
-         return false;
-      }
+   // function validaDigitosCPF(cpf) {
+   //    // Valida os dígitos verificadores
+   //    let soma = 0;
+   //    let resto;
+   //    for (let i = 1; i <= 9; i++) {
+   //       soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+   //    }
+   //    resto = (soma * 10) % 11;
+   //    if (resto === 10 || resto === 11) {
+   //       resto = 0;
+   //    }
+   //    if (resto !== parseInt(cpf.substring(9, 10))) {
+   //       return false;
+   //    }
 
-      soma = 0;
-      for (let i = 1; i <= 10; i++) {
-         soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
-      }
-      resto = (soma * 10) % 11;
-      if (resto === 10 || resto === 11) {
-         resto = 0;
-      }
-      if (resto !== parseInt(cpf.substring(10, 11))) {
-         return false;
-      }
+   //    soma = 0;
+   //    for (let i = 1; i <= 10; i++) {
+   //       soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+   //    }
+   //    resto = (soma * 10) % 11;
+   //    if (resto === 10 || resto === 11) {
+   //       resto = 0;
+   //    }
+   //    if (resto !== parseInt(cpf.substring(10, 11))) {
+   //       return false;
+   //    }
 
-      return true;
-   }
+   //    return true;
+   // }
 
-   function validaUf() {
+   function validaEstado() {
       let objTemp = {
          validado: valSucesso,
          mensagem: [],
@@ -401,45 +395,45 @@ export default function Cadastro() {
       return testeResult;
    }
 
-   function validaComplemento() {
-      let objTemp = {
-         validado: valSucesso,
-         mensagem: [],
-      };
+   // function validaComplemento() {
+   //    let objTemp = {
+   //       validado: valSucesso,
+   //       mensagem: [],
+   //    };
 
-      setValida((prevState) => ({
-         ...prevState, // mantém os valores anteriores
-         complemento: objTemp, // atualiza apenas o campo 'nome'
-      }));
+   //    setValida((prevState) => ({
+   //       ...prevState, // mantém os valores anteriores
+   //       complemento: objTemp, // atualiza apenas o campo 'nome'
+   //    }));
 
-      const testeResult = objTemp.mensagem.length === 0 ? 1 : 0;
-      return testeResult;
-   }
+   //    const testeResult = objTemp.mensagem.length === 0 ? 1 : 0;
+   //    return testeResult;
+   // }
 
-   function validaCelular() {
-      let objTemp = {
-         validado: valSucesso,
-         mensagem: [],
-      };
+   // function validaCelular() {
+   //    let objTemp = {
+   //       validado: valSucesso,
+   //       mensagem: [],
+   //    };
 
-      if (usuario.cli_cel === "") {
-         objTemp.validado = valErro;
-         objTemp.mensagem.push("O nº do celular é obrigatório");
-      } else if (usuario.cli_cel.length < 11) {
-         objTemp.validado = valErro;
-         objTemp.mensagem.push(
-            "O número do celular deve ter pelo menos 11 dígitos"
-         );
-      }
+   //    if (usuario.cli_cel === "") {
+   //       objTemp.validado = valErro;
+   //       objTemp.mensagem.push("O nº do celular é obrigatório");
+   //    } else if (usuario.cli_cel.length < 11) {
+   //       objTemp.validado = valErro;
+   //       objTemp.mensagem.push(
+   //          "O número do celular deve ter pelo menos 11 dígitos"
+   //       );
+   //    }
 
-      setValida((prevState) => ({
-         ...prevState, // mantém os valores anteriores
-         celular: objTemp, // atualiza apenas o campo 'nome'
-      }));
+   //    setValida((prevState) => ({
+   //       ...prevState, // mantém os valores anteriores
+   //       celular: objTemp, // atualiza apenas o campo 'nome'
+   //    }));
 
-      const testeResult = objTemp.mensagem.length === 0 ? 1 : 0;
-      return testeResult;
-   }
+   //    const testeResult = objTemp.mensagem.length === 0 ? 1 : 0;
+   //    return testeResult;
+   // }
 
    function validaSenha() {
       let objTemp = {
@@ -491,22 +485,21 @@ export default function Cadastro() {
       event.preventDefault();
       let itensValidados = 0;
       itensValidados += validaNome();
-      itensValidados += validaNascimento();
+      itensValidados += validaApelido();
       itensValidados += validaEmail();
-      itensValidados += validaUf();
-      itensValidados += validaCidade();
+      itensValidados += validaSenha();
+      itensValidados += validaConfSenha();
+      itensValidados += validaCnpj();
+      itensValidados += validaCep();
       itensValidados += validaLogradouro();
       itensValidados += validaNumero();
       itensValidados += validaBairro();
-      itensValidados += validaComplemento();
-      itensValidados += validaCelular();
-      itensValidados += validaSenha();
-      itensValidados += validaConfSenha();
-      itensValidados += validaCpf();
+      itensValidados += validaCidade();
+      itensValidados += validaEstado();
 
       // salvar quando atingir o número de itens a serem validados
       // alert(itensValidados);
-      if (itensValidados === 13) {
+      if (itensValidados === 12) {
          // alert('chama api');
 
          try {
@@ -697,6 +690,23 @@ export default function Cadastro() {
                         width={25}
                         height={25}
                         alt="Icone Numero"
+                        className={styles.Icons}
+                     />
+                  </div>
+
+                  <div className={styles.FormGroup}>
+                     <input
+                        type="Bairro"
+                        id="Bairro"
+                        name="Bairro"
+                        placeholder="Digite seu Bairro"
+                        className={styles.InputField}
+                     />
+                     <Image
+                        src="/icones/IconeEndereco.svg"
+                        width={25}
+                        height={25}
+                        alt="Icone Bairro"
                         className={styles.Icons}
                      />
                   </div>
